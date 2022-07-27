bb90512f757d9360b3d7457b755c9a75d8c66dff
Richard Lane <richard.lane@bristol.ac.uk>
2022-07-27 10:56:49 +0100

    create some pickled dumps of DataFrames for our momenta and times - it doesn't quite look consistent yet, though i'm committing now because i'm getting annoyed at it

---
+++ create_dumps.py
@@ -0,0 +1,137 @@
+"""
+Read momenta and decay times into an array for LHCb monte carlo (signal MC)
+and AmpGen
+
+For now, uses the same straight cuts to isolate the signal as the background
+classifier - TODO find a better solution for this, or something
+
+"""
+import sys
+import pickle
+import pathlib
+from multiprocessing import Process
+from typing import List
+import pandas as pd
+import numpy as np
+import uproot
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1] / "k3pi_signal_cuts"))
+
+from lib_cuts import read_data, definitions, cuts
+
+
+def _ampgen_df(tree, sign: str) -> pd.DataFrame:
+    """
+    Populate a pandas dataframe with momenta and time arrays from the provided tree
+
+    """
+    df = pd.DataFrame()
+
+    t_branch = "Dbar0_decayTime" if sign == "RS" else "D0_decayTime"
+    df["time"] = tree[t_branch].array() * 1000  # Convert to ps
+
+    suffixes = "Px", "Py", "Pz", "E"
+
+    # Note the order of the branches here
+    # To make it consistent with the MC, I have swapped the order of the pions so that we have K+pi-pi+pi-
+    # TODO no i havent
+    branches = [
+        *(f"_1_K~_{s}" for s in suffixes),
+        *(f"_2_pi#_{s}" for s in suffixes),
+        *(f"_3_pi#_{s}" for s in suffixes),
+        *(f"_4_pi~_{s}" for s in suffixes),
+    ]
+
+    for branch in branches:
+        df[branch] = tree[branch].array() * 1000  # Convert to MeV
+
+    return df
+
+
+def _mc_df(tree) -> pd.DataFrame:
+    """
+    Populate a pandas dataframe with momenta and time arrays from the provided tree
+
+    """
+    df = pd.DataFrame()
+    keep = cuts.keep(tree)
+
+    df["time"] = read_data.decay_times(tree)[keep]
+    suffixes = "P_X", "P_Y", "P_Z", "P_E"
+
+    # Use truth value momenta, instead of refit TODO see if that's right
+    mc_branches = [
+        *(f"D0_P0_TRUE{s}" for s in suffixes),
+        *(f"D0_P1_TRUE{s}" for s in suffixes),
+        *(f"D0_P2_TRUE{s}" for s in suffixes),
+        *(f"D0_P3_TRUE{s}" for s in suffixes),
+    ]
+    for branch in mc_branches:
+        # Take the first value from each entry in the jagged array - this is the best fit value
+        df[branch] = tree[branch].array()[keep]
+
+    return df
+
+
+def _create_dump(
+    gen: np.random.Generator,
+    files: List[str],
+    sign: str = None,
+    mc: bool = False,
+    train_fraction: float = 0.5,
+) -> None:
+    """
+    Create pickle dump of the momenta and decay times for D -> K3pi events.
+    AmpGen evts are all K+3pi - the equivalent phase space distribution for D -> K-3pi can be found by flipping the
+    3-momenta. Maybe I'll write a helper function for this
+
+    :param gen: random number generator for train/test split
+    :param sign: "RS" or "WS" - must be specified for MC. tells us which tree to read from the file
+    :param files: iterable of filepaths to read from
+    :param background: bool flag telling us whether this data is MC or AmpGen - we need to apply cuts to MC
+    :param train_fraction: how much data to set aside for testing/training.
+
+    """
+    if sign is None and mc:
+        raise ValueError("Must provide sign for reading MC")
+
+    df_fcn = _mc_df if mc else lambda tree: _ampgen_df(tree, sign)
+    tree_name = definitions.tree_name(sign) if mc else "DalitzEventList"
+
+    dfs = []
+    for root_file in files:
+        with uproot.open(root_file) as f:
+            # TODO get the right tree for ampgen
+            df = df_fcn(f[tree_name])
+            df["train"] = gen.random(len(df)) < train_fraction
+
+            dfs.append(df)
+
+    # Dump it
+    path = (
+        "mc.pkl" if mc else "ampgen.pkl"
+    )  # TODO nicer. Could use the definitions fcn, extend it to take ampgen
+    with open(path, "wb") as f:
+        print(f"dumping {path}")
+        pickle.dump(pd.concat(dfs), f)
+
+
+def main(year: str, sign: str, mc: bool) -> None:
+    """
+    Create a pickle dump
+
+    """
+    files = (
+        definitions.mc_files(year, "magdown") if mc else definitions.ampgen_files(sign)
+    )
+    gen = np.random.default_rng(seed=0)
+    _create_dump(gen, files, sign, mc)
+
+
+if __name__ == "__main__":
+    procs = [Process(target=main, args=("2018", "RS", mc)) for mc in (True, False)]
+
+    for p in procs:
+        p.start()
+    for p in procs:
+        p.join()
---
+++ scripts/plot_parameterisation.py
@@ -0,0 +1,90 @@
+"""
+Make plots of the variables that will be used for the reweighting
+
+This script doesn't require a reweighter to exist - it's intended to be used to check that you've created
+the dataframes correctly.
+
+"""
+import sys
+import pathlib
+import pickle
+from typing import Tuple
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from fourbody.param import helicity_param
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / "k3pi_signal_cuts"))
+
+from lib_cuts.read_data import momentum_order
+
+
+def _k_3pi(
+    dataframe: pd.DataFrame,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Get the kaon and 3 pions as 4xN numpy arrays of (px, py, pz, E)
+
+    """
+    # TODO make this nicer by having consistently named column headings in the dataframes
+    try:
+        suffixes = "P_X", "P_Y", "P_Z", "P_E"
+        particles = [
+            (f"D0_P0_TRUE{s}" for s in suffixes),
+            (f"D0_P1_TRUE{s}" for s in suffixes),
+            (f"D0_P3_TRUE{s}" for s in suffixes),
+            (f"D0_P2_TRUE{s}" for s in suffixes),
+        ]
+        return tuple(
+            np.row_stack([dataframe[x] for x in labels]) for labels in particles
+        )
+
+    except KeyError:
+        suffixes = "Px", "Py", "Pz", "E"
+        particles = [
+            (f"_1_K~_{s}" for s in suffixes),
+            (f"_2_pi#_{s}" for s in suffixes),
+            (f"_3_pi#_{s}" for s in suffixes),
+            (f"_4_pi~_{s}" for s in suffixes),
+        ]
+        return tuple(
+            np.row_stack([dataframe[x] for x in labels]) for labels in particles
+        )
+
+
+def main():
+    """
+    Create a plot
+
+    """
+    with open("ampgen.pkl", "rb") as f:
+        ampgen_df = pickle.load(f)
+    with open("mc.pkl", "rb") as f:
+        mc_df = pickle.load(f)
+
+    ag_k, ag_pi1, ag_pi2, ag_pi3 = _k_3pi(ampgen_df)
+    mc_k, mc_pi1, mc_pi2, mc_pi3 = _k_3pi(mc_df)
+
+    # TODO should momentum order when creating dataframes
+    ag_pi1, ag_pi2 = momentum_order(ag_k, ag_pi1, ag_pi2)
+    mc_pi1, mc_pi2 = momentum_order(mc_k, mc_pi1, mc_pi2)
+
+    ag = np.column_stack(
+        (helicity_param(ag_k, ag_pi1, ag_pi2, ag_pi3), ampgen_df["time"])
+    )
+    mc = np.column_stack((helicity_param(mc_k, mc_pi1, mc_pi2, mc_pi3), mc_df["time"]))
+
+    fig, ax = plt.subplots(2, 3, figsize=(12, 8))
+
+    hist_kw = {"density": True, "histtype": "step"}
+    for axis, ag_x, mc_x in zip(ax.ravel(), ag.T, mc.T):
+        _, bins, _ = axis.hist(ag_x, bins=100, label="AG", **hist_kw)
+        axis.hist(mc_x, bins=bins, label="MC", **hist_kw)
+
+    ax[0, 0].legend()
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
