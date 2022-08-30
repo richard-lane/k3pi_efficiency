add functionality for getting both K+ and K- dataframes in the common script - this actually fixed a bug where i didnt get the sign right for particle gun, so now the plots look much better

scripts/common.py
@@ -20,16 +20,24 @@ def ampgen_df(sign: str, k_sign: str) -> pd.DataFrame:
 
     """
     assert sign in {"dcs", "cf"}
-    assert k_sign in {"k_plus", "k_minus"}
+    assert k_sign in {"k_plus", "k_minus", "both"}
 
     dataframe = get.ampgen("dcs" if sign == "false" else sign)
     dataframe = dataframe[~dataframe["train"]]
 
-    if k_sign == "k_minus":
-        dataframe = util.flip_momenta(
-            dataframe, np.ones(len(dataframe), dtype=np.bool_)
-        )
+    if k_sign == "k_plus":
+        # Don't flip any momenta
+        return dataframe
 
+    if k_sign == "k_minus":
+        # Flip all the momenta
+        mask = np.ones(len(dataframe), dtype=np.bool_)
+
+    elif k_sign == "both":
+        # Flip half of the momenta randomly
+        mask = np.random.random(len(dataframe)) < 0.5
+
+    dataframe = util.flip_momenta(dataframe, mask)
     return dataframe
 
 
@@ -51,7 +59,10 @@ def pgun_df(sign: str, k_sign: str) -> pd.DataFrame:
 
     else:
         dataframe = get.particle_gun(sign, show_progress=True)
+
         # We only want test data here
         dataframe = dataframe[~dataframe["train"]]
+
+        dataframe = efficiency_util.k_sign_cut(dataframe, k_sign)
 
     return dataframe

scripts/plot_time_ratio.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / "k3pi-data"))
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[0]))
 
+import common
 from lib_data import get, util
 
 from lib_efficiency import (
@@ -36,19 +38,8 @@ def _times_and_weights(
     Get testing times and weights
 
     """
-    ampgen_df = get.ampgen(sign)
-    pgun_df = get.particle_gun(sign, show_progress=True)
-
-    # We only want test data here
-    pgun_df = pgun_df[~pgun_df["train"]]
-    ampgen_df = ampgen_df[~ampgen_df["train"]]
-
-    # Deal with getting rid of evts/flipping momenta if we need to
-    pgun_df = efficiency_util.k_sign_cut(pgun_df, data_sign)
-    if data_sign == "k_minus":
-        ampgen_df = util.flip_momenta(
-            ampgen_df, np.ones(len(ampgen_df), dtype=np.bool_)
-        )
+    ampgen_df = common.ampgen_df(sign, data_sign)
+    pgun_df = common.pgun_df(sign, data_sign)
 
     # Just pass the arrays into the efficiency function and it should find the right weights
     mc_k, mc_pi1, mc_pi2, mc_pi3 = efficiency_util.k_3pi(pgun_df)
@@ -136,7 +127,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "data_sign",
         type=str,
-        choices={"k_plus", "k_minus"},
+        choices={"k_plus", "k_minus", "both"},
         help="whether to read K+ or K- data",
     )
     parser.add_argument(